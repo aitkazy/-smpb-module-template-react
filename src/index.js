cfc6c6da4ce412d5d9d72073d0a27fc7ac207edd
yerassyl <aitkazy.smartex@gmail.com>
2021-01-29 14:19:11 +0600

    startup settings and layouts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { App } from './App';
+import { AppGlobalsProvider } from './contexts/appGlobals';
 
-const App = () => {
-    return <div className="title">Rollup-React template for building moduled app</div>;
-};
+export class AppInstance {
+    constructor(_boostrap = {}) {
+        if (!_boostrap.rootElement) throw new Error('Укажите rootElement!');
 
-export const render = roorElement => {
-    return ReactDOM.render(<App />, roorElement);
+        this._rootElement = _boostrap.rootElement;
+        this._settings = _boostrap.settings;
+        this._result = null;
+        this._setResult = this._setResult.bind(this);
+    }
+
+    beforeMount() {
+    }
+
+    beforeUnmount() {
+    }
+
+    _setResult(result) {
+        this._result = result;
+    }
+
+    mount() {
+        this.beforeMount();
+        render(
+            <AppGlobalsProvider
+                onResult={this._setResult}
+                appSettings={this._settings}
+            >
+                <App />
+            </AppGlobalsProvider>,
+            this._rootElement
+        );
+    }
+
+    unmount() {
+        this.beforeUnmount(this._result);
+        unmountComponentAtNode(this._rootElement);
+    }
 };
